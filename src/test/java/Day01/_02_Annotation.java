1e39b0429e3231c029009b9b21faa13136a03988
14403 <a7la_soko@yahoo.com>
2023-02-24 20:36:31 -0500

    test

--- src/test/java/Day01/_02_Annotation.java
+++ src/test/java/Day01/_02_Annotation.java
@@ -1,4 +1,45 @@
 package Day01;
 
+import org.testng.annotations.*;
+
 public class _02_Annotation {
+
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("before method is working");
+    }
+
+
+    @Test
+    void test1(){
+        System.out.println("so good" );
+    }
+
+
+    @Test
+    void test2(){
+        System.out.println("good");
+    }
+
+        @BeforeMethod
+    public void beforeMethod2(){
+            System.out.println("AAAAAAAAAAAAAAAA");
+        }
+
+        @AfterMethod
+    public void afterMethod(){
+            System.out.println("after");
+        }
+
+
+        @AfterClass
+                public void afterClass(){
+            System.out.println("after class ");
+        }
+
+        @BeforeClass
+    public void BeforeClass(){
+            System.out.println("before class");
+        }
 }
